0d0a18b442c0d7dd219d7991ee7ab27193057407
Brian Birtles <birtles@gmail.com>
2022-04-02 22:22:17 +0900

    chore: add playwright test for download

---
+++ src/download-v2.test.ts
@@ -0,0 +1,102 @@
+import chai, { assert } from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import fetchMock from 'fetch-mock';
+
+import { download, DownloadEvent } from './download-v2';
+
+mocha.setup('bdd');
+chai.use(chaiAsPromised);
+
+const VERSION_1_0_0 = {
+  kanji: {
+    '1': {
+      major: 1,
+      minor: 0,
+      patch: 0,
+      databaseVersion: '175',
+      dateOfCreation: '2019-07-09',
+    },
+  },
+};
+
+describe('download', () => {
+  afterEach(() => fetchMock.restore());
+
+  it('should download the initial version information', async () => {
+    fetchMock.mock('end:version-en.json', VERSION_1_0_0);
+    fetchMock.mock(
+      'end:kanji/en/1.0.0.jsonl',
+      `{"type":"header","version":{"major":1,"minor":0,"patch":0,"databaseVersion":"2019-173","dateOfCreation":"2019-06-22"},"records":0,"format":"full"}
+`
+    );
+
+    const abortController = new AbortController();
+    const events = await drainEvents(
+      download({
+        lang: 'en',
+        forceFetch: true,
+        majorVersion: 1,
+        series: 'kanji',
+        signal: abortController.signal,
+      })
+    );
+
+    assert.deepEqual(events, [
+      {
+        type: 'version',
+        major: 1,
+        minor: 0,
+        patch: 0,
+        databaseVersion: '2019-173',
+        dateOfCreation: '2019-06-22',
+      },
+      { type: 'versionend' },
+    ]);
+  });
+});
+
+async function drainEvents(
+  downloader: AsyncIterableIterator<DownloadEvent>,
+  {
+    includeProgressEvents = false,
+    wrapError = false,
+  }: { includeProgressEvents?: boolean; wrapError?: boolean } = {}
+): Promise<Array<DownloadEvent>> {
+  const events: Array<DownloadEvent> = [];
+
+  try {
+    for await (const event of downloader) {
+      if (includeProgressEvents || event.type !== 'progress') {
+        events.push(event);
+      }
+    }
+  } catch (e) {
+    if (wrapError) {
+      throw new DrainError(e, events);
+    } else {
+      throw e;
+    }
+  }
+
+  return events;
+}
+
+// If we get an error while draining, we should return the error along with all
+// the events read up until that point.
+class DrainError extends Error {
+  error: unknown;
+  events: Array<DownloadEvent>;
+
+  constructor(error: unknown, events: Array<DownloadEvent>, ...params: any[]) {
+    super(...params);
+    Object.setPrototypeOf(this, DrainError.prototype);
+
+    if (typeof (Error as any).captureStackTrace === 'function') {
+      (Error as any).captureStackTrace(this, DrainError);
+    }
+
+    this.name = 'DrainError';
+    this.error = error;
+    this.events = events;
+  }
+}
