733d5e0dc9ff2e9c812c9e6c715add337224bbe5
Brian Birtles <birtles@gmail.com>
2022-05-13 12:36:33 +0900

    feat: add CJS version

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,14 @@ import typescript from '@rollup/plugin-typescript';
 import cleaner from 'rollup-plugin-cleaner';
 import dts from 'rollup-plugin-dts';
 
+const external = [
+  '@birchill/json-equalish',
+  '@birchill/normal-jp',
+  'idb/with-async-ittr',
+  'safari-14-idb-fix',
+  'superstruct',
+];
+
 export default [
   {
     input: 'src/index.ts',
@@ -10,18 +18,22 @@ export default [
       format: 'es',
       sourcemap: true,
     },
-    external: [
-      '@birchill/json-equalish',
-      '@birchill/normal-jp',
-      'idb/with-async-ittr',
-      'safari-14-idb-fix',
-      'superstruct',
-    ],
+    external,
     plugins: [cleaner({ targets: ['dist/'] }), typescript({ outDir: 'dist' })],
   },
   {
     input: 'src/index.ts',
     output: [{ file: 'dist/index.d.ts', format: 'es' }],
     plugins: [dts()],
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.cjs',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    external,
+    plugins: [typescript({ declaration: false })],
   },
 ];
