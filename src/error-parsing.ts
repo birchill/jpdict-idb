948c681e99452c65761dc442af65370c9596a27b
Brian Birtles <birtles@gmail.com>
2025-12-05 15:58:48 +0900

    fix: make return type of isAbortError slightly more accurate

--- src/error-parsing.ts
+++ src/error-parsing.ts
@@ -2,7 +2,9 @@ import type { AbortError } from './abort-error.js';
 import type { DownloadError } from './download-error.js';
 import { isObject } from './is-object.js';
 
-export function isAbortError(e: unknown): e is AbortError {
+type AbortDomException = DOMException & { name: 'AbortError' };
+
+export function isAbortError(e: unknown): e is AbortError | AbortDomException {
   return isObject(e) && 'name' in e && e.name === 'AbortError';
 }
 
